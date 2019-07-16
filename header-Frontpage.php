43a648ba7aa0b2b033d0a1a8c4a709c68fad9468
trolund@gmail.com <vxh77kdp>
2019-07-16 16:14:17 +0200

    video banner virker

--- header-Frontpage.php
+++ header-Frontpage.php
@@ -13,6 +13,8 @@
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script>
         // conditionizr.com
         // configure environment tests
@@ -22,39 +24,43 @@
         });
         </script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/VideoBackground.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
 
 	</head>
-	<body <?php body_class(); ?>>
+	<body <?php body_class(); ?> onresize="adaptHeaderHeight()">
 
-    <video autoplay muted loop id="myVideo">
-        <source src="<?php echo get_template_directory_uri(); ?>/video/rain.mp4" type="video/mp4">
-        Your browser does not support HTML5 video.
-    </video>
+
 
 		<!-- wrapper -->
 		<div class="wrapper overlay">
 
 			<!-- header -->
 			<header class="header clear" role="banner">
-                <div class="headerContainer">
 
+                <div class="overlay"></div>
+                <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+                    <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+                </video>
+                <div class="container">
+                    <div class="topContainer">
+                    <!-- logo -->
+                    <div class="logo">
+                        <a href="<?php echo home_url(); ?>">
+                            <!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+                        </a>
+                    </div>
+                    <!-- /logo -->
 
-
-                         <!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-                            <p>new</p>
-						</a>
-					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
+                    <!-- nav -->
+                    <nav class="nav" role="navigation">
+                        <?php html5blank_nav(); ?>
+                    </nav>
+                    <!-- /nav -->
+                    </div>
+                    <div class="frontText"><span><b>NITROGEN SENSOR FOR SOIL SUSTAINABILITY</b></span></div>
+                    </div>
                 </div>
 			</header>
+
 			<!-- /header -->
--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,15 @@
 		'use strict';
 		
 		// DOM ready, take it away
+
+
 		
 	});
 	
 })(jQuery, this);
+
+function adaptHeaderHeight() {
+	var height = $("#myVideo").height;
+	window.console.log(Console);
+	$(".header").css("height", height +200);
+}
